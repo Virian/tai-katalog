ec09c9d390b4a0708f966db7e6742ce9b8729120
KasiaDzimira <katarzyna.dzimira@onet.pl>
2018-11-08 21:39:43 +0100

    initialize app and configuration

---
+++ front-end/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Switch>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
