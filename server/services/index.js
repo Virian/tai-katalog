56481772288378bfa151e24e83dad390759ac6fc
Grzegorz Stoklosa <stoklosa.grzegorz@itsilesia.com>
2018-12-03 18:18:27 +0100

    Add uploading and listing photos on backend

--- server/services/index.js
+++ server/services/index.js
@@ -1,7 +1,11 @@
+const imageService = require('./ImageService');
+const exifService = require('./ExifService');
 const userService = require('./UserService');
 const errorHandler = require('./ErrorHandler');
 
 module.exports = {
+  imageService: imageService,
+  exifService: exifService,
   userService: userService,
-  errorHandler: errorHandler
+  errorHandler: errorHandler,
 };
