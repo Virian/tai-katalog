f09ffbe27f92c8cabd988fcc1e67af97efe020af
Grzegorz Stoklosa <gstok@poczta.fm>
2019-01-12 17:04:42 +0100

    Add image classification on backend

--- server/services/ImageService.js
+++ server/services/ImageService.js
@@ -1,3 +1,4 @@
+const exec = require('child_process').exec;
 const { Image } = require('../models');
 
 function save (data) {
@@ -54,10 +55,36 @@ function generateUrl (req) {
   return req.protocol + "://" + host + ':' + port + '/' + filePath;
 }
 
+function classifyImage (filePath, callback) {
+  let cmd = 'python classify_image.py --image_file ' + filePath
+  exec(cmd, function(error, stdout, stderr) {
+    let classesString = '';
+    if (error) console.log(stderr);
+    else classesString = getGoogleInceptionClasses(stdout, 0.1);
+    callback(classesString);
+  });
+}
+
+function getGoogleInceptionClasses (input, threshold) {
+  let returnString = '';
+  const classes = input.split('\r\n');
+  for (classString of classes) {
+    if (classString.length === 0) continue;
+    const eqIdx = classString.lastIndexOf('=');
+    const score = parseFloat(classString.slice(eqIdx + 2, classString.length - 2));
+    if (score < threshold) continue;
+    if (returnString.length > 0) returnString += ', ';
+    returnString += classString.slice(0, classString.indexOf('(') - 1);
+  }
+  return returnString;
+}
+
 module.exports = {
   save: save,
   update:update,
   find: find,
   findOne: findOne,
   generateUrl: generateUrl,
+  classifyImage: classifyImage,
+  getGoogleInceptionClasses: getGoogleInceptionClasses
 }
